b2df6fb68eb272e5642f467f16afb0ab31382763
Wagner Camarao <wagner@camarao.io>
2013-09-07 13:33:25 -0700

    rails new grifter --database=postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grifter::Application.config.secret_key_base = '6a5ac38e3ee55338436e50877451159f35a017c14dc75cdcc66e4c4ddce3f19d8b798f2e5a2090c6e26b493dc96ab14c11fef6661c87138bd80bda3d04293b2f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grifter::Application.config.session_store :cookie_store, key: '_grifter_session'
