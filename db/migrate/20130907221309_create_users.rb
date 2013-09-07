fc0eb9b08a8cd4121e349a2461983f1515b59bb0
Wagner Camarao <wagner@camarao.io>
2013-09-07 15:33:04 -0700

    Fix migration

--- db/migrate/20130907221309_create_users.rb
+++ db/migrate/20130907221309_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.bigint :fbuid
+      t.integer :fbuid, :limit => 8
       t.decimal :latitude
       t.decimal :longitude
 
