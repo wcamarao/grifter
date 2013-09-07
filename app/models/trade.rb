f95bed488ba847c6a1bb5db52b92fbcfccaddf96
Wagner Camarao <wagner@camarao.io>
2013-09-07 16:59:08 -0700

    Set rels

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,2 +1,6 @@
 class Trade < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :item
+
+  validates :status, :inclusion => %w(POSTED ACCEPTED COMPLETED CANCELLED)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Grifter::Application.routes.draw do
   resources :trades
-
   resources :items
-
   resources :users
+
+  root 'items#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
