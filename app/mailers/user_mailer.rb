688e4d65ad6dff4ee1827f7a18caf595bb7f5dc7
Kamran <kamranjon@gmail.com>
2013-09-08 09:00:03 -0700

    making the to email correct

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,11 @@ class UserMailer < ActionMailer::Base
   default from: 'grift@grifter.io'
 
   def trade_email(trade)
-    @user = trade.user
+    @from_user = trade.user
+    @to_user = trade.item.user
     @trade = trade
     @url = "/trades/#{trade.id}"
 
-    puts "email: #{@user.email}"
-
-    mail(to: @user.email, subject: 'Grift Request!')
+    mail(to: @to_user.email, subject: 'Grift Request!')
   end
 end
