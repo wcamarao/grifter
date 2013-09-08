780b777cec45e7a534f01813a0dd7eda19361d01
Wagner Camarao <wagner@camarao.io>
2013-09-08 08:31:11 -0700

    Nav

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,6 @@ class Item < ActiveRecord::Base
   belongs_to :user
   before_create :upload_picture
 
-  validates_presence_of :name
-
   def upload_picture
     id = flickr.upload_photo picture.path, :title => picture.original_filename
     info = flickr.photos.getInfo(:photo_id => id)
