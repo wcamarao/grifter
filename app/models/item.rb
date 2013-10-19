b8640121946c39ffaa14708929adc1f43a0dc9ec
Wagner Camarao <wagner@camarao.io>
2013-10-19 09:10:03 -0700

    Cascade delete/update flickr pictures

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ require 'flickraw'
 class Item < ActiveRecord::Base
   MILES_TO_METERS_RATIO = 1609.34
   before_create :set_default_status
+  before_update :delete_previous_picture
+  before_destroy :delete_picture
   before_save :upload_picture
 
   validates :name, :picture, presence: true
@@ -40,6 +42,18 @@ class Item < ActiveRecord::Base
     self.status = 'Active'
   end
 
+  def delete_previous_picture
+    return unless picture.present? && changes.include?('picture')
+    flickr_id = JSON.parse(changed_attributes['picture'])['id']
+    flickr.photos.delete(:photo_id => flickr_id)
+  end
+
+  def delete_picture
+    return unless picture.present?
+    flickr_id = JSON.parse(picture)['id']
+    flickr.photos.delete(:photo_id => flickr_id)
+  end
+
   def upload_picture
     return unless picture.present? && (new_record? || changes.include?('picture'))
     flickr_id = flickr.upload_photo(picture.path, :title => picture.original_filename)
@@ -49,14 +63,15 @@ class Item < ActiveRecord::Base
 
   def hashify_picture(flickr_info)
     {
-      n: FlickRaw.url_n(flickr_info),
-      z: FlickRaw.url_z(flickr_info),
-      o: FlickRaw.url_o(flickr_info)
+      'id' => flickr_info.id,
+      'n' => FlickRaw.url_n(flickr_info),
+      'z' => FlickRaw.url_z(flickr_info),
+      'o' => FlickRaw.url_o(flickr_info)
     }
   end
 
   def parse_picture(size)
-    return unless self.picture.present?
-    JSON.parse(self.picture)[size]
+    return unless picture.present?
+    JSON.parse(picture)[size]
   end
 end
